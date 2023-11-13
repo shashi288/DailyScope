1a03fa183ba60d6c6105477734ab3baa1bfd1bd2
Shashi <shashisharma882002@gmail.com>
2023-11-14 02:28:48 +0530

    path updated in package.json

--- src/App.js
+++ src/App.js
@@ -8,8 +8,8 @@ import LoadingBar from 'react-top-loading-bar'
 
 const App = ()=> {
   const pageSize = 5;
-  const apiKey = process.env.REACT_APP_NEWS_API
-  const [progress, setProgress] = useState(0)
+  const apiKey = process.env.REACT_APP_NEWS_API;
+  const [progress, setProgress] = useState(0);
  
     return (
       <div>
